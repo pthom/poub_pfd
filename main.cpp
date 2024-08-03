80b063c8b264e683f01ba80640fa4863b1648d7d
Pascal Thomet <pthomet@gmail.com>
2024-08-03 21:36:03 +0200

    s

--- main.cpp
+++ main.cpp
@@ -1,15 +1,12 @@
-#include <iostream>
-#include "portable_file_dialogs/portable_file_dialogs.h"
-
+#include "stdio.h"
+#include <future>
 
 int main()
 {
-    std::cout << "1\n"; 
-    auto dlg = pfd::open_file("File Dialog");
-    std::cout << "2\n"; 
-    auto res = dlg.result();
-    std::cout << "3\n"; 
-    if (res.size() > 0)
-        std::cout << res[0] << "\n";
-    std::cout << "4\n"; 
+    printf("main enter\n");
+    std::mutex m_mutex;
+    printf("Created mutex\n");
+    std::unique_lock<std::mutex> lock(m_mutex);
+    printf("Instantiated lock\n");
+    printf("main exit\n");
 }
