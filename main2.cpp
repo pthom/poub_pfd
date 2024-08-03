0322728c1c52bfecd5b2cb5d1a2b90847d439f85
Pascal Thomet <pthomet@gmail.com>
2024-08-03 21:28:57 +0200

    v5 gh fvvc

--- main2.cpp
+++ main2.cpp
@@ -7,19 +7,19 @@ int main()
 {
     printf("main enter\n");
     std::mutex m_mutex;
-    std::future<int> m_future;
+    //std::future<int> m_future;
 
-    auto trampoline = []()
-    {
-        printf("trampoline 1\n");
-        return 0;
-    };
+    //auto trampoline = []()
+    //{
+    //    printf("trampoline 1\n");
+    //    return 0;
+    //};
     printf("main 2\n");
 
     std::unique_lock<std::mutex> lock(m_mutex);
     printf("main 3\n");
-    m_future = std::async(std::launch::async, trampoline);
-    printf("main 4\n");
-    m_future.get();
+    //m_future = std::async(std::launch::async, trampoline);
+    //printf("main 4\n");
+    //m_future.get();
     printf("main exit\n");
 }
