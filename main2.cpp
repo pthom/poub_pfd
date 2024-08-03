79d53e6ce34ac914d39548d5cdac6624e5b011d2
Pascal Thomet <pthomet@gmail.com>
2024-08-03 21:31:56 +0200

    v5 gh fvvvc

--- main2.cpp
+++ main2.cpp
@@ -1,25 +1,12 @@
-#include <iostream>
+#include "stdio.h"
 #include <future>
-#include "portable_file_dialogs/portable_file_dialogs.h"
-
 
 int main()
 {
     printf("main enter\n");
     std::mutex m_mutex;
-    //std::future<int> m_future;
-
-    //auto trampoline = []()
-    //{
-    //    printf("trampoline 1\n");
-    //    return 0;
-    //};
-    printf("main 2\n");
-
+    printf("Created mutex\n");
     std::unique_lock<std::mutex> lock(m_mutex);
-    printf("main 3\n");
-    //m_future = std::async(std::launch::async, trampoline);
-    //printf("main 4\n");
-    //m_future.get();
+    printf("Instantiated lock\n");
     printf("main exit\n");
 }
