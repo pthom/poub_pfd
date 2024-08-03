ab1ab13427819b8f4b70697136863b8dca1d3dfe
Pascal Thomet <pthomet@gmail.com>
2024-08-03 21:17:53 +0200

    v5 gh fc

--- main2.cpp
+++ main2.cpp
@@ -6,32 +6,32 @@
 inline void start_func()
 {
     printf("start_func enter\n");
-    //printf("start_func 2\n");
-    //std::mutex m_mutex;
+    std::mutex m_mutex;
     std::future<int> m_future;
-    std::condition_variable m_cond;
+    //std::condition_variable m_cond;
 
-    auto trampoline = [&m_cond]()
+    auto trampoline = []()
     {
         printf("trampoline 1\n");
-        m_cond.notify_one();
+        //m_cond.notify_one();
         printf("trampoline 2\n");
         return 0;
     };
     printf("start_func 3\n");
 
-    //std::unique_lock<std::mutex> lock(m_mutex);
+    std::unique_lock<std::mutex> lock(m_mutex);
     printf("start_func 3\n");
     m_future = std::async(std::launch::async, trampoline);
     printf("start_func 4\n");
     //m_cond.wait(lock);
+    m_future.get();
     printf("start_func 5\n");
 }
 
 
 int main()
 {
-    std::cout << "1\n"; 
+    std::cout << "main enter\n"; 
     start_func();
-    std::cout << "4\n"; 
+    std::cout << "main end\n"; 
 }
